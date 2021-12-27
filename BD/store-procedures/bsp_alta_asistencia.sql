624e9bf8a81ca7d241c7a5bae4faaca493610337
Your Name <you@example.com>
2021-12-27 17:16:13 -0300

    modificacion formulario + sp en BD

---
+++ BD/store-procedures/bsp_alta_asistencia.sql
@@ -0,0 +1,40 @@
+DROP procedure IF EXISTS `bsp_alta_asistencia`;
+DELIMITER $$
+CREATE PROCEDURE `bsp_alta_asistencia`(pDNI INT,pIdEscuela INT,pApellidos VARCHAR(60),pNombres VARCHAR(60))
+SALIR:BEGIN
+	/*
+    Permite dar de alta una persona por su DNI. 
+    Devuelve OK + Id o el mensaje de error en Mensaje.
+    */
+	DECLARE pIdPersonal, pIdAsistencia int;
+	-- Manejo de error en la transacción
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		-- SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador.' Mensaje,
+				NULL AS Id;
+		ROLLBACK;
+	END;
+    -- Controla que la categoría sea obligatoria 
+	IF pDNI = '' OR pDNI IS NULL THEN
+		SELECT 'Debe proveer un DNI' AS Mensaje, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+    -- Controla que no exista un DNI repetido
+	IF EXISTS(SELECT DNI FROM Personal WHERE DNI = pDNI) THEN
+		SELECT 'Ya existe un DNI con ese numero' AS Mensaje;
+		LEAVE SALIR;
+    END IF;
+	START TRANSACTION;
+		SET pIdPersonal = 1 + (SELECT COALESCE(MAX(IdPersonal),0)
+								FROM personal);
+		SET pIdAsistencia = 1 + (SELECT COALESCE(MAX(IdAsistencia),0)
+								FROM asistencias);
+                                
+		INSERT INTO personal VALUES(pIdPersonal, pIdEscuela,pApellidos,pNombres);
+        INSERT INTO asistencias VALUES(pIdAsistencia,pIdPersonal, now(),null, '-');
+        
+        SELECT 'OK' AS Mensaje;
+    COMMIT;
+END$$
+DELIMITER ;
--- educatec/CapaPresentacion/formAsistencia.Designer.cs
+++ educatec/CapaPresentacion/formAsistencia.Designer.cs
@@ -31,7 +31,11 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formAsistencia));
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -46,27 +50,63 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtNombres);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtApellidos);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtDNI);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(479, 112);
+            this.groupBox1.Size = new System.Drawing.Size(479, 216);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Nueva asistencia";
+            this.groupBox1.Text = "Datos personales";
             // 
-            // textBox1
+            // txtDNI
             // 
-            this.textBox1.Location = new System.Drawing.Point(123, 42);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(211, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtDNI.Location = new System.Drawing.Point(123, 42);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(211, 20);
+            this.txtDNI.TabIndex = 1;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Location = new System.Drawing.Point(123, 90);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(211, 20);
+            this.txtApellidos.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese Apellidos : ";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(123, 145);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(211, 20);
+            this.txtNombres.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ingrese Nombres : ";
             // 
             // formAsistencia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 140);
+            this.ClientSize = new System.Drawing.Size(507, 240);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "formAsistencia";
@@ -81,6 +121,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.Label label2;
     }
 }
