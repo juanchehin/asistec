642e1606081cf20091a140fd1193a749c2ddde4f
Your Name <you@example.com>
2021-12-27 17:43:43 -0300

    Nuevo SP

---
+++ BD/store-procedures/bsp_dame_asistencias_por_dia.sql
@@ -0,0 +1,37 @@
+DROP procedure IF EXISTS `bsp_dame_asistencias_por_dia`;
+DELIMITER $$
+CREATE PROCEDURE `bsp_dame_asistencias_por_dia`(pFecha date)
+SALIR:BEGIN
+	/*
+    Permite listar las asistencias de una fecha dada, devuelve una tabla
+    con el listado de los DNI, apellidos ,nombres y escuela
+    */
+	-- Manejo de error en la transacción
+	DECLARE EXIT HANDLER FOR SQLEXCEPTION
+	BEGIN
+		SHOW ERRORS;
+		SELECT 'Error en la transacción. Contáctese con el administrador.' Mensaje,
+				NULL AS Id;
+		ROLLBACK;
+	END;
+    -- Controla que la fecha sea obligatoria 
+	IF pFecha = '' OR pFecha IS NULL THEN
+		SELECT 'Debe proveer una fecha' AS Mensaje, NULL AS Id;
+		LEAVE SALIR;
+    END IF;
+
+	START TRANSACTION;
+	SELECT		p.DNI,p.Apellidos,p.Nombres,e.Escuela
+    FROM		asistencias a 
+				LEFT JOIN personal p on a.Id_personal = p.Id
+                LEFT JOIN escuelas e on e.Id = p.Id_Escuela
+	WHERE		a.FechaEntrada = pFecha
+	GROUP BY	p.Id
+    ORDER BY	a.Id asc;
+    -- LIMIT 		pDesde,5;
+        
+	SELECT 'OK' AS Mensaje;
+    
+    COMMIT;
+END$$
+DELIMITER ;
