c84de5e8b5e60e83c75f751990e3733ed3cd9e06
Your Name <you@example.com>
2021-12-27 17:02:51 -0300

    formulacion de inicio

--- educatec/CapaPresentacion/formAsistencias.Designer.cs
+++ educatec/CapaPresentacion/formAsistencias.Designer.cs
@@ -46,7 +46,6 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::educatec.Properties.Resources.educatec;
             this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(116, 82);
