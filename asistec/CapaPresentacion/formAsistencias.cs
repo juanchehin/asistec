712f8b935cc73a854269c8f9dc1771f1042db9fe
chehin <chehinjuan@gmail.com>
2022-01-04 08:58:31 -0300

    exportacion de asistencias en excel

--- asistec/CapaPresentacion/formAsistencias.cs
+++ asistec/CapaPresentacion/formAsistencias.cs
@@ -39,66 +39,56 @@ namespace educatec.CapaPresentacion
 
         private void btnExcel_Click(object sender, EventArgs e)
         {
-            SLDocument s1 = new SLDocument();
+            SLDocument sl = new SLDocument();
             SLStyle style = new SLStyle();
             style.Font.FontSize = 12;
             style.Font.Bold = true;
 
-            s1.SetCellValue(1, 1, "DNI");
-            s1.SetCellValue(1, 2, "Apellidos");
-            s1.SetCellValue(1, 3, "Nombres");
-            s1.SetCellValue(1, 4, "Escuela");
-            s1.SetCellValue(1, 5, "HorarioEntrada");
-            s1.SetCellValue(1, 6, "HorarioSalida");
+            sl.SetCellValue(1, 1, "DNI");
+            sl.SetCellValue(1, 2, "Apellidos");
+            sl.SetCellValue(1, 3, "Nombres");
+            sl.SetCellValue(1, 4, "Escuela");
+            sl.SetCellValue(1, 5, "HorarioEntrada");
+            sl.SetCellValue(1, 6, "HorarioSalida");
 
-            s1.SetCellStyle(1,1,style);
-            s1.SetCellStyle(1, 2, style);
-            s1.SetCellStyle(1, 3, style);
-            s1.SetCellStyle(1, 4, style);
-            s1.SetCellStyle(1, 5, style);
-            s1.SetCellStyle(1, 6, style);
+            sl.SetCellStyle(1,1,style);
+            sl.SetCellStyle(1, 2, style);
+            sl.SetCellStyle(1, 3, style);
+            sl.SetCellStyle(1, 4, style);
+            sl.SetCellStyle(1, 5, style);
+            sl.SetCellStyle(1, 6, style);
 
-            int fila = 1;
-            int columna = 0;
-            // int totalFilas = dataListadoAsistencias.Rows.Count;
+            int fila = 0;
 
-            for(int i = 0; i < dataListadoAsistencias.Rows.Count; i++ )
+            for(int i = 2; i < (dataListadoAsistencias.Rows.Count + 2 ); i++ )
             {
+                sl.SetCellValue(i, 1, dataListadoAsistencias.Rows[fila].Cells["DNI"].Value.ToString());
+                sl.SetCellValue(i, 2, dataListadoAsistencias.Rows[fila].Cells["Apellidos"].Value.ToString());
+                sl.SetCellValue(i, 3, dataListadoAsistencias.Rows[fila].Cells["Nombres"].Value.ToString());
+                sl.SetCellValue(i, 4, dataListadoAsistencias.Rows[fila].Cells["Escuela"].Value.ToString());
+                sl.SetCellValue(i, 5, dataListadoAsistencias.Rows[fila].Cells["HorarioEntrada"].Value.ToString());
+                sl.SetCellValue(i, 6, dataListadoAsistencias.Rows[fila].Cells["HorarioSalida"].Value.ToString());
 
-                // s1.SetCellValue(i, 0, dataListadoAsistencias.Cells[fila].Value.ToString());
-                s1.SetCellValue(i, 1, dataListadoAsistencias.Rows[i].Cells[1].Value.ToString());
-                s1.SetCellValue(i, 2, dataListadoAsistencias.Rows[i].Cells[2].Value.ToString());
-                s1.SetCellValue(i, 3, dataListadoAsistencias.Rows[i].Cells[3].Value.ToString());
-                s1.SetCellValue(i, 4, dataListadoAsistencias.Rows[i].Cells[4].Value.ToString());
-                s1.SetCellValue(i, 5, dataListadoAsistencias.Rows[i].Cells[5].Value.ToString());
-                // s1.SetCellValue(i, 5, dataListadoAsistencias.Rows[i].Cells[6].Value.ToString());
-                Console.WriteLine(" Rows[i].Cells[1] " + dataListadoAsistencias.Rows[i].Cells[1].Value.ToString());
-                Console.WriteLine(" -- Salto -- " + i);
-
-                /*foreach (DataGridViewRow row in dataListadoAsistencias.Rows)
-                {
-                    Console.WriteLine("Ahora va el cell : " + row.Cells[fila].Value.ToString());
-                    
-                    s1.SetCellValue(fila, columna, row.Cells[fila].Value.ToString());
-                    columna++;
-                }
-                Console.WriteLine(" -- Salto -- ");
-                fila++;*/
+                fila++;
             }
 
-            // int iC = 1;
-            // int iR = 1;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Title = "Guardar archivo";
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "xlsx";
 
-            /*foreach (DataGridViewColumn column in dataListadoAsistencias.Columns)
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                s1.SetCellValue(1, iC, column.HeaderText.ToString());
-                s1.SetCellValue(1, 2, "columna 2");
-                s1.SetCellStyle(1, iC,style);                
-            }*/
-
-
-
-            s1.SaveAs("C:/Users/Chehin/Documents/asistencias.xlsx");
+                try
+                {
+                    sl.SaveAs(saveFileDialog1.FileName);
+                    MessageBox.Show("Archivo exportado con éxito");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
