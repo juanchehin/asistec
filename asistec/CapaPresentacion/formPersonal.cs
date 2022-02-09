f452602b86d29c5a2805bbff20a24c2db184184c
Your Name <you@example.com>
2022-02-09 17:29:08 -0300

    ingreso de DNI solo numerico en busqueda personal

--- asistec/CapaPresentacion/formPersonal.cs
+++ asistec/CapaPresentacion/formPersonal.cs
@@ -83,13 +83,28 @@ namespace educatec.CapaPresentacion
         }
         private void BuscarPersonal()
         {
-            Console.WriteLine("this.txtBuscar.Text es " + this.txtDNI.Text);
             this.dataListadoPersonal.DataSource = objetoCL.BuscarPersonal(Convert.ToInt32(this.txtDNI.Text));
             lblTotalPersonal.Text = Convert.ToString(dataListadoPersonal.Rows.Count);
         }
 
         private void txtDNI_KeyPress(object sender, KeyPressEventArgs e)
         {
+            if (Char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else if (Char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
             if ((int)e.KeyChar == (int)Keys.Enter)
             {
                 this.BuscarPersonal();
